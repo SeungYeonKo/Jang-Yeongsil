bb3141e976e9b3afd97aa3d87e86cbb652e27e25
KoSeungYeon <createnewscene479@gmail.com>
2024-07-18 15:41:18 +0900

    [고승연] 타이머 빼고 모든 퀴즈 완성

--- Assets/02.Scripts/HistoryQuiz/MultipleSelectType.cs
+++ Assets/02.Scripts/HistoryQuiz/MultipleSelectType.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class MultipleSelectType : MonoBehaviour
 {
-    public ToggleGroup[] ToggleGroups; 
-    public Toggle[] AnswerToggles; 
+    public ToggleGroup[] ToggleGroups;
+    public Toggle[] AnswerToggles;
     public Image[] CorrectImages;
     public Image[] WrongImages;
 
@@ -14,16 +15,8 @@ public class MultipleSelectType : MonoBehaviour
 
     private void OnEnable()
     {
-        // 시작할 때마다 모든 토글 초기화(는잘안됨 왜지?)
-        foreach (ToggleGroup group in ToggleGroups)
-        {
-            foreach (Toggle toggle in group.GetComponentsInChildren<Toggle>())
-            {
-                // 토글을 강제로 초기화해보겠음->안됨,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-                toggle.isOn = true;
-                toggle.isOn = false;
-            }
-        }
+        // 시작할 때 모든 토글 초기화
+        StartCoroutine(ResetTogglesCoroutine());
     }
 
     void Start()
@@ -41,7 +34,10 @@ public class MultipleSelectType : MonoBehaviour
 
         CloseButton.onClick.AddListener(CloseButtonClick);
         SubmitButton.onClick.AddListener(SubmitButtonClick);
-        ResetButton.onClick.AddListener(SubmitButtonClick);
+        ResetButton.onClick.AddListener(ResetButtonClick);
+
+        // 시작할 때 모든 토글 초기화
+        ResetToggles();
     }
 
     // 제출 버튼
@@ -81,6 +77,41 @@ public class MultipleSelectType : MonoBehaviour
 
     public void ResetButtonClick()
     {
+        // 이미지 비활성화
+        foreach (Image img in CorrectImages)
+        {
+            img.gameObject.SetActive(false);
+        }
 
+        foreach (Image img in WrongImages)
+        {
+            img.gameObject.SetActive(false);
+        }
+
+        // 모든 토글 체크 해제
+        ResetToggles();
+    }
+
+    private void ResetToggles()
+    {
+        foreach (ToggleGroup group in ToggleGroups)
+        {
+            group.allowSwitchOff = true; // 모든 토글을 해제할 수 있도록 설정
+
+            foreach (Toggle toggle in group.GetComponentsInChildren<Toggle>())
+            {
+                toggle.isOn = false;
+                // 상태 변경 이벤트 수동 트리거
+                toggle.onValueChanged.Invoke(toggle.isOn);
+            }
+
+            group.allowSwitchOff = false; // 원래 상태로 되돌림
+        }
+    }
+
+    private IEnumerator ResetTogglesCoroutine()
+    {
+        yield return null; // 프레임이 한 번 업데이트된 후 실행
+        ResetToggles();
     }
 }

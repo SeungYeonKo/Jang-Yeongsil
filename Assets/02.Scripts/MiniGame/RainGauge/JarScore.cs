ffd12ee1acbdc6629dcc2ca5b3d05645ad1ffa2a
조희수 <hs.jo.sw.p@gmail.com>
2024-07-22 09:37:52 +0900

    [조희수] 레디한 플레이어가 2명 이상일 때 시작

--- Assets/02.Scripts/MiniGame/RainGauge/JarScore.cs
+++ Assets/02.Scripts/MiniGame/RainGauge/JarScore.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class JarScore : MonoBehaviour
 {
-    
+    public static JarScore Instance { get; private set; }
+
+    public GameObject Jar1;
+    public GameObject Jar2;
+    public GameObject Jar3;
+    public GameObject Jar4;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 }

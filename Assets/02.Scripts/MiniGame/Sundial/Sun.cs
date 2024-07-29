19e22a968cece04a1c517f87d511d248bff95ca6
surnamemin <surnamemin@gmail.com>
2024-07-29 15:47:40 +0900

    [이성민] 태양이랑 슬라이더 연동 (완) 이제 퀴즈로직만 짜면 끝

--- Assets/02.Scripts/MiniGame/Sundial/Sun.cs
+++ Assets/02.Scripts/MiniGame/Sundial/Sun.cs
@@ -17,8 +17,8 @@ public class Sun : MonoBehaviour
 
     void OnSliderValueChanged(float value)
     {
-        // 슬라이더 값에 따라 회전 값을 계산
-        float rotationValue = Mathf.Lerp(minRotation, maxRotation, value);
+        // 슬라이더 값이 0에서 500으로 변할 때 회전 값을 160에서 30으로 매핑
+        float rotationValue = Mathf.Lerp(minRotation, maxRotation, value / 500f);
 
         // 새로운 회전 값 설정, Y와 Z 값은 90도로 고정
         transform.rotation = Quaternion.Euler(rotationValue, 90f, 90f);

1257ad428e0f4fb1362beb11edfb4dca1d576f0e
surnamemin <surnamemin@gmail.com>
2024-08-04 17:26:19 +0900

    [이성민] 미니게임로직

---
+++ Assets/02.Scripts/MiniGame/Sundial/SunMiniGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMiniGame : MonoBehaviour
+{
+    public ClockInteraction clockInteraction;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}

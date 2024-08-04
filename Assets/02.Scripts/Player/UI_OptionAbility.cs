478af40a811146cff1341913d9e26e7c4c0975fb
조희수 <hs.jo.sw.p@gmail.com>
2024-08-04 15:38:19 +0900

    [조희수] 측우기점수포톤

---
+++ Assets/02.Scripts/Player/UI_OptionAbility.cs
@@ -0,0 +1,109 @@
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_OptionAbility : MonoBehaviourPunCallbacks
+{
+   public static UI_OptionAbility Instance { get; private set; }
+   
+   private PlayerOptionAbility _playerOptionAbility;
+   
+   void Awake()
+   {
+      if (Instance == null)
+      {
+         Instance = this;
+         DontDestroyOnLoad(gameObject);
+      }
+      else
+      {
+         Destroy(gameObject);
+      }
+   }
+
+   private void Start()
+   {
+      gameObject.SetActive(false);
+
+      if (photonView.IsMine)
+      {
+         _playerOptionAbility = FindObjectOfType<PlayerOptionAbility>();
+      }
+   }
+   private void Update()
+   {      
+      if (Input.GetKeyDown(KeyCode.Escape))
+      {
+          ToggleUI();
+      }
+   }
+   private void ToggleUI()
+   {
+      bool isActive = !gameObject.activeSelf;
+      gameObject.SetActive(isActive);
+      UnityEngine.Cursor.visible = isActive;
+      UnityEngine.Cursor.lockState = isActive ? CursorLockMode.None : CursorLockMode.Locked;
+      if (photonView.IsMine)
+      {
+         if (isActive)
+         {
+            _playerOptionAbility.Pause();
+         }
+         else
+         {
+            _playerOptionAbility.Continue();
+         }
+      }
+   }
+   public void OnClickXbutton()
+   {
+      gameObject.SetActive(false);
+      UnityEngine.Cursor.visible = false;
+      UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+      _playerOptionAbility.Continue();
+   }
+   public void OnClickReplay()
+   {
+      gameObject.SetActive(false);
+      UnityEngine.Cursor.visible = false;
+      UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+      _playerOptionAbility.Continue();
+   }
+   
+   public void OnClickSquare()
+   {
+      gameObject.SetActive(false);
+      UnityEngine.Cursor.visible = false;
+      UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+      if (SceneManager.GetActiveScene().name == "MainScene")
+      {
+         OnClickReplay();
+      }
+      else
+      {
+         PhotonNetwork.LeaveRoom();
+         PhotonNetwork.LoadLevel("MainScene");
+      }
+   }
+   
+   public void OnClickGameQuitButton()
+   {
+      gameObject.SetActive(false);
+      UnityEngine.Cursor.visible = false;
+      UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+      if (_playerOptionAbility!= null)
+      {
+         // Photon Network에서 방 나가기
+         PhotonNetwork.LeaveRoom();
+      }
+
+      // 빌드 후 실행됐을 경우 종료하는 방법
+      Application.Quit();
+
+#if UNITY_EDITOR
+      // 유니티 에디터에서 실행했을 경우 종료하는 방법
+      UnityEditor.EditorApplication.isPlaying = false;
+#endif
+   }
+   
+}

adbcfd0488184aa16f99d3ac1c636e81ef3c959a
KoSeungYeon <createnewscene479@gmail.com>
2024-08-04 16:09:36 +0900

    [고승연] 풀용 커밋

--- Assets/02.Scripts/Museum/GlobalInventionManager.cs
+++ Assets/02.Scripts/Museum/GlobalInventionManager.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class GlobalInventionManager : MonoBehaviour
 {
-    public static Hashtable InventionState = new Hashtable();
-    public static Hashtable QuickSlotState = new Hashtable(); // 퀵슬롯 상태 저장
+    public static Dictionary<string, bool> InventionState = new Dictionary<string, bool>();
 
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
 
-        // 초기 상태 설정 (필요에 따라 기본값 설정)
+        // 모든 발명품의 초기 상태를 false로 설정
         InventionState["Sundial"] = false;
         InventionState["ArmillarySphere"] = false;
         InventionState["Cheugugi"] = false;
@@ -31,21 +30,7 @@ public class GlobalInventionManager : MonoBehaviour
     {
         if (InventionState.ContainsKey(inventionName))
         {
-            return (bool)InventionState[inventionName];
-        }
-        return false;
-    }
-
-    public static void SaveQuickSlotState(InventionType inventionType, bool isActive)
-    {
-        QuickSlotState[inventionType] = isActive;
-    }
-
-    public static bool GetQuickSlotState(InventionType inventionType)
-    {
-        if (QuickSlotState.ContainsKey(inventionType))
-        {
-            return (bool)QuickSlotState[inventionType];
+            return InventionState[inventionName];
         }
         return false;
     }
--- Assets/02.Scripts/Museum/MuseumManager.cs
+++ Assets/02.Scripts/Museum/MuseumManager.cs
@@ -12,6 +12,7 @@ public class MuseumManager : MonoBehaviour
         {
             string inventionName = obj.name;
 
+            // 발명품 상태를 확인하여 활성화
             if (GlobalInventionManager.IsInventionActive(inventionName))
             {
                 obj.SetActive(true);
@@ -20,6 +21,8 @@ public class MuseumManager : MonoBehaviour
             {
                 obj.SetActive(false);
             }
+
+            Debug.Log($"Invention: {inventionName}, Active: {GlobalInventionManager.IsInventionActive(inventionName)}");
         }
     }
 }

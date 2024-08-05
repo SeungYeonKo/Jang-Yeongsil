c542b11c31ec0be3948b3e178d4667c910567245
조희수 <hs.jo.sw.p@gmail.com>
2024-08-05 13:53:05 +0900

    [조희수] 2명 이상&& 방에 입장한 사람 전부 레디했을 때만

--- Assets/02.Scripts/MiniGame/RainGauge/RainGaugeManager.cs
+++ Assets/02.Scripts/MiniGame/RainGauge/RainGaugeManager.cs
@@ -245,6 +245,12 @@ public class RainGaugeManager : MonoBehaviourPunCallbacks
         //Debug.Log("Player count: " + players.Length);
         int readyPlayerCount = 0;
 
+
+        if (players.Length < 2)
+        {
+            return false; 
+        }
+
         foreach (Photon.Realtime.Player player in players)
         {
             if (player.CustomProperties.TryGetValue("IsReady_RainGauge", out object isReadyObj))
@@ -253,26 +259,10 @@ public class RainGaugeManager : MonoBehaviourPunCallbacks
                 {
                     readyPlayerCount++;
                 }
-                else
-                {
-                    //Debug.Log("플레이어가 준비되지 않았습니다: " + player.NickName);
-                }
-            }
-            else
-            {
-                //Debug.Log("플레이어 준비 상태가 없습니다: " + player.NickName);
             }
         }
-        if (readyPlayerCount >= 1)
-        {
-            //Debug.Log("플레이어 모두 레디");
-            return true;
-        }
-        else
-        {
-            //Debug.Log("레디한 플레이어 수가 충분하지 않습니다.");
-            return false;
-        }
+
+        return readyPlayerCount == players.Length;
     }
 
     private void UpdateGameTimer()

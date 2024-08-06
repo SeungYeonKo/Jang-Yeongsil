9486dfffe8b9431f07d2da741f4b5521c49218f8
surnamemin <surnamemin@gmail.com>
2024-08-06 16:36:17 +0900

    [이성민] 마우스커서 활성화 비활성화 코드 수정

--- Assets/02.Scripts/MiniGame/Sundial/ClockInteraction.cs
+++ Assets/02.Scripts/MiniGame/Sundial/ClockInteraction.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine.SceneManagement; // 씬 관리를 위해 필요
+using UnityEngine.SceneManagement;
 
 public class ClockInteraction : MonoBehaviour
 {
@@ -10,27 +10,18 @@ public class ClockInteraction : MonoBehaviour
     public Canvas sundialSliderCanvas; // 전체 UI 캔버스 (SundialSlider_Canvas)
     public Slider sundialSlider; // 슬라이더 오브젝트
     public Image sunImage; // 슬라이더 이미지 오브젝트
-    
+
     public TextMeshProUGUI qKeyText; // Q 키 텍스트
     public Image additionalImage; // 추가 이미지 오브젝트
     public SliderImageAnimator sliderImageAnimator; // SliderImageAnimator 스크립트 참조
 
-    
     private bool isNearClock = false;
     private bool isClockViewActive = false;
 
     public SunMiniGame SunMiniGame;
 
-
     void Awake()
     {
-        // 현재 씬이 "SundialScene"이 아닌 경우, 스크립트 비활성화
-        if (SceneManager.GetActiveScene().name != "SundialScene")
-        {
-            this.enabled = false;
-            return;
-        }
-
         // SunMiniGame 스크립트와의 연결을 설정합니다.
         if (SunMiniGame != null)
         {
@@ -41,38 +32,48 @@ public class ClockInteraction : MonoBehaviour
 
     private void Start()
     {
-        // 초기 설정: 미니게임 카메라 비활성화, 개별 UI 오브젝트 비활성화
         ResetMiniGame();
     }
 
     void Update()
     {
-        // 해시계 근처에 있을 때 Q 키 입력을 받으면
         if (isNearClock && Input.GetKeyDown(KeyCode.Q))
         {
             SunMiniGame.StartMiniGame();
             SunMiniGame.qzText.gameObject.SetActive(true);
             ToggleClockView();
         }
+
+        if (SunMiniGame != null)
+        {
+            if (SunMiniGame.isGameActive)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
     }
 
     public void ResetMiniGame()
     {
-        // 미니게임 종료 시 초기 상태로 복원
         if (miniGameCamera != null) miniGameCamera.gameObject.SetActive(false);
         if (sundialSlider != null) sundialSlider.gameObject.SetActive(false);
         if (sunImage != null) sunImage.gameObject.SetActive(false);
-        
         if (additionalImage != null) additionalImage.gameObject.SetActive(false);
-
-        // 시작할 때 마우스 커서 숨김
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
 
         if (mainCamera != null) mainCamera.gameObject.SetActive(true);
         if (qKeyText != null) qKeyText.gameObject.SetActive(false);
 
         isClockViewActive = false;
+
+        // 미니게임이 종료되면 기본 TPSCamera의 커서 설정으로 돌아감
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void ToggleClockView()
@@ -96,7 +97,6 @@ public class ClockInteraction : MonoBehaviour
                 sunImage.gameObject.SetActive(true);
                 sliderImageAnimator.displayImage.sprite = sliderImageAnimator.sprites[0];
             }
-            
             if (additionalImage != null)
             {
                 additionalImage.gameObject.SetActive(true);
@@ -113,24 +113,18 @@ public class ClockInteraction : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        // Sundial 태그를 가진 오브젝트와 충돌했을 때
         if (other.CompareTag("Player"))
         {
             isNearClock = true;
-
-            // Q 키 텍스트 활성화
             if (qKeyText != null) qKeyText.gameObject.SetActive(true);
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        // Sundial 태그를 가진 오브젝트가 범위를 벗어났을 때
         if (other.CompareTag("Player"))
         {
             isNearClock = false;
-
-            // Q 키 텍스트 비활성화
             if (qKeyText != null) qKeyText.gameObject.SetActive(false);
 
             SunMiniGame.isGameActive = false;

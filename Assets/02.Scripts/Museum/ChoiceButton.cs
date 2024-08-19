2941a06f8409810031558675a5034ccacf20e109
BALL-GGUYA-PC\jisoo <3dperson1@gmail.com>
2024-08-19 15:28:37 +0900

    [공지수] 뮤지엄씬 해설,동영상 버튼 추가 / 각 버튼에 맞게 설정

---
+++ Assets/02.Scripts/Museum/ChoiceButton.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class ChoiceButton : MonoBehaviour
+{
+    public GameObject Description;
+    public GameObject Video;
+    public PlayableDirector playableDirector;
+    public TextMeshProUGUI IntroText;
+
+    void Start()
+    {
+        IntroText.gameObject.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        Description.SetActive(false);
+        Video.SetActive(false);
+    }
+    
+    public void ShowDescription()
+    {
+        IntroText.gameObject.SetActive(false);
+        Video.SetActive(false);
+        Description.SetActive(true);
+    }
+
+    public void ClosePopup()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ShowVideo()
+    {
+        IntroText.gameObject.SetActive(false);
+        Description.SetActive(false);
+        Video.SetActive(true);
+        playableDirector.Play();
+    }
+}

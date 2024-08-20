5f759cfaa4308c9260150969450db8ffda1c2a71
BALL-GGUYA-PC\jisoo <3dperson1@gmail.com>
2024-08-20 16:50:44 +0900

    [공지수] 투호에서 비석치기로 급선회 / 스톤태그붙은 큐브 플레이어 손에 붙어서 드래그로 던지기 / 애니메이션 완성

--- Assets/02.Scripts/RockGame/ThrowAnim.cs
+++ Assets/02.Scripts/RockGame/ThrowAnim.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using ExitGames.Client.Photon;
 using UnityEngine;
 
 public class ThrowAnim : MonoBehaviour
@@ -14,22 +15,26 @@ public class ThrowAnim : MonoBehaviour
 
     void HandleMouseInput()
     {
-        if (Input.GetMouseButtonDown(0) && IsInPrepareThrowPose())
+        // Animator의 현재 상태가 "Idle"일 때만 드래그 가능
+        if (IsInIdleState() && Input.GetMouseButtonDown(0))
         {
             isDragging = true;
             animator.SetBool("IsDragging", true); // 드래그 시작
+            Debug.Log("드래그 중");
         }
 
         if (Input.GetMouseButtonUp(0) && isDragging)
         {
             isDragging = false;
             animator.SetBool("IsDragging", false); // 드래그 종료, Throw 상태로 전환
+            Debug.Log("드래그 끝");
         }
     }
 
-    bool IsInPrepareThrowPose()
+    // 현재 Animator 상태가 "Idle"인지 확인하는 함수
+    bool IsInIdleState()
     {
-        // Animator의 현재 상태가 PrepareThrow일 때만 드래그를 허용
-        return animator.GetCurrentAnimatorStateInfo(0).IsName("PrepareThrow");
+        // "Base Layer"의 0번째 레이어에서 현재 애니메이션 상태를 가져옵니다.
+        return animator.GetCurrentAnimatorStateInfo(0).IsName("Idle");
     }
 }

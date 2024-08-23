751de0f9fe2990abc8912c1fdcc1f4a863e5a58c
KoSeungYeon <createnewscene479@gmail.com>
2024-08-23 14:09:23 +0900

    [고승연] 후후

--- Assets/02.Scripts/MiniGame/Clepsydra/StartTrigger.cs
+++ Assets/02.Scripts/MiniGame/Clepsydra/StartTrigger.cs
@@ -1,31 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class StartTrigger : MonoBehaviour
 {
     public Image StartImage;
+    public Button StartButton;
+    public Transform Maze1StartPosition;
+
+    private GameObject player;
+
+    private void Start()
+    {
+        StartButton.onClick.AddListener(OnStartButtonClick);
+    }
+
+    public void OnStartButtonClick()
+    {
+        if (player != null && Maze1StartPosition != null)
+        {
+            player.transform.position = Maze1StartPosition.position;
+        }
+
+        if (StartImage != null)
+        {
+            StartImage.gameObject.SetActive(false);
+        }
+
+        if (StartButton != null)
+        {
+            StartButton.gameObject.SetActive(false);
+        }
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if(StartImage != null)
+            if (StartImage != null)
             {
                 StartImage.gameObject.SetActive(true);
             }
+
+            if (StartButton != null)
+            {
+                StartButton.gameObject.SetActive(true);
+            }
+
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+
+            player = other.gameObject;
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if(StartImage != null)
+            if (StartImage != null)
             {
-                StartImage.gameObject.SetActive(false); 
+                StartImage.gameObject.SetActive(false);
             }
+
+            if (StartButton != null)
+            {
+                StartButton.gameObject.SetActive(false);
+            }
+
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+
+            player = null;
         }
     }
 }

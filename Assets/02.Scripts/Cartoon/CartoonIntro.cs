3a3938640e5e419e45b325647866cedabc0a6c00
BALL-GGUYA-PC\jisoo <3dperson1@gmail.com>
2024-09-03 16:15:44 +0900

    [공지수] 웹툰 씬 만들고 코루틴 넣음

---
+++ Assets/02.Scripts/Cartoon/CartoonIntro.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartoonIntro : MonoBehaviour
+{
+    public List<GameObject> CartoonImg;
+
+    private void Start()
+    {
+        foreach (var img in CartoonImg)
+        {
+            img.SetActive(false);
+        }
+        StartCoroutine(Cartoon_Coroutine());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            
+        }
+    }
+
+    IEnumerator Cartoon_Coroutine()
+    {
+        for (int i = 0; i < CartoonImg.Count; i++)
+        {
+            CartoonImg[i].SetActive(true);
+            yield return new WaitForSeconds(3.5f);
+            if (i < CartoonImg.Count - 1)
+            {
+                CartoonImg[i].SetActive(false);
+            }
+        }
+    }
+}

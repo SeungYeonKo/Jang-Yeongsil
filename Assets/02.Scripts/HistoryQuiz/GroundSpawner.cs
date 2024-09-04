6e07359629095da42eb66fc83f226b2d0ef8af14
KoSeungYeon <createnewscene479@gmail.com>
2024-09-04 09:07:22 +0900

    [고승연] 퀴즈 노선 틀었음 하던 씬 삭제(빠듯한 일정 관계로,,)

---
+++ Assets/02.Scripts/HistoryQuiz/GroundSpawner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour
+{
+    public GameObject GroundTile;
+    Vector3 NextSpawnPoint;
+
+    void SpawnTile()
+    {
+
+    }
+
+    private void Start()
+    {
+        
+    }
+}

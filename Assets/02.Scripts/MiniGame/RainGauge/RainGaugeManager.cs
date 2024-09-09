be6db082bd217816c71203e1582fc211fdd86945
조희수 <hs.jo.sw.p@gmail.com>
2024-09-10 01:33:05 +0900

    [조희수] 비는 Go 상태에서만

--- Assets/02.Scripts/MiniGame/RainGauge/RainGaugeManager.cs
+++ Assets/02.Scripts/MiniGame/RainGauge/RainGaugeManager.cs
@@ -33,6 +33,8 @@ public class RainGaugeManager : MonoBehaviourPunCallbacks
     private Dictionary<int, RainGaugePlayer> players = new Dictionary<int, RainGaugePlayer>();
     private UI_RainGaugeManager uiRainGaugeManager;
 
+    private ParticleSystem rainParticleSystem;
+
     private void Awake()
     {
         if (Instance == null)
@@ -184,6 +186,16 @@ public class RainGaugeManager : MonoBehaviourPunCallbacks
 
         SoundManager.instance.StopBgm();
         SoundManager.instance.PlayBgm(SoundManager.Bgm.RainGauge);
+
+        GameObject waterfallEffect = GameObject.Find("WaterfallSmallEffect");
+        if (waterfallEffect != null)
+        {
+            rainParticleSystem = waterfallEffect.GetComponent<ParticleSystem>();
+            if (rainParticleSystem != null)
+            {
+                rainParticleSystem.gameObject.SetActive(false); 
+            }
+        }
     }
 
     private void Update()
@@ -199,10 +211,12 @@ public class RainGaugeManager : MonoBehaviourPunCallbacks
 
             case GameState.Loading:
                 uiRainGaugeManager.SetReadyImageState(false);
+                SetRainParticleSystemActive(false);
                 break;
 
             case GameState.Go:
                 uiRainGaugeManager.SetReadyImageState(false);
+                SetRainParticleSystemActive(true);
                 UpdateGameTimer();
                 if (TimeRemaining > 0)
                 {
@@ -217,6 +231,7 @@ public class RainGaugeManager : MonoBehaviourPunCallbacks
 
             case GameState.Over:
                 uiRainGaugeManager.SetReadyImageState(false);
+                SetRainParticleSystemActive(false);
                 if (!_isGameOver)
                 {
                     _isGameOver = true;
@@ -231,6 +246,15 @@ public class RainGaugeManager : MonoBehaviourPunCallbacks
             int localPlayerNumber = (int)PhotonNetwork.LocalPlayer.CustomProperties["PlayerNumber"];
             PlayerPrefs.SetInt("WinnerPlayerNumber", localPlayerNumber); // 승자 정보 저장
             SetGameState(GameState.Over); // 게임 종료
+        }
+    }
+
+    private void SetRainParticleSystemActive(bool isActive)
+    {
+        if (rainParticleSystem != null && rainParticleSystem.gameObject.activeSelf != isActive)
+        {
+            rainParticleSystem.gameObject.SetActive(isActive);
+            Debug.Log($"RainParticleSystem is now {(isActive ? "enabled" : "disabled")}");
         }
     }
 
